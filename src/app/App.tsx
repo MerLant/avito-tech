2c7a8ce974d90b41d5fc266f41287a85f4b3a6d9
MerLant <m3rlant@gmail.com>
2025-03-06 20:55:33 +0500

    Fix bugs

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,12 +2,11 @@ import React from "react";
 import { ChakraProvider, theme } from "@chakra-ui/react";
 import AppRouter from "./router";
 import { Layout } from "src/shared/ui";
-import { Header } from "src/widgets/ui";
 
 export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Layout>
-      <AppRouter />
-    </Layout>
-  </ChakraProvider>
+	<ChakraProvider theme={theme}>
+		<Layout>
+			<AppRouter />
+		</Layout>
+	</ChakraProvider>
 );
